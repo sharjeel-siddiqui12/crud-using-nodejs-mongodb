e0e1ed9c7ced7fa00295f81b5b9bec9d86729ee9
sharjeel-siddiqui12 <sharjeelsiddiqui00@gmail.com>
2024-09-18 15:54:23 +0500

    Refactor package.json and index.mjs
    - Update start script in package.json to use index.mjs instead of server.mjs
    - Add dev script in package.json to use nodemon for development
    - Remove server.mjs file

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const app = express()
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5001;
 
 app.get('/water', (req, res) => {
     console.log(`${req.ip} is asking for water`)
