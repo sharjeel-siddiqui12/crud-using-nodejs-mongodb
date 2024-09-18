0efd44489ac5496ecfe68724510c959b17ef0be9
sharjeel-siddiqui12 <sharjeelsiddiqui00@gmail.com>
2024-09-18 15:57:43 +0500

    Add CORS middleware for cross-origin resource sharing

--- index.mjs
+++ index.mjs
@@ -1,7 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express()
 const port = process.env.PORT || 5001;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     console.log(`${req.ip} is asking for hello`)
